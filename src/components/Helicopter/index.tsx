6f3bd8fec6808ccc3e64639de79c775092729ce9
GRND CTRL <mjrtom@grndctrl.io>
2024-01-15 17:56:56 +0100

    its alive

--- src/components/Helicopter/index.tsx
+++ src/components/Helicopter/index.tsx
@@ -1,64 +1,75 @@
-import { MathUtils, Raycaster, Vector3 } from "three";
+import { Euler, MathUtils, Quaternion, Raycaster, Vector3 } from "three";
 import { Cone } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { Ray } from '@dimforge/rapier3d-compat'
-import { CapsuleCollider, RigidBody, RapierRigidBody, useRapier, interactionGroups } from "@react-three/rapier";
+import { Ray } from "@dimforge/rapier3d-compat";
+import {
+  CapsuleCollider,
+  RigidBody,
+  RapierRigidBody,
+  useRapier,
+  interactionGroups,
+} from "@react-three/rapier";
 import { useControls } from "leva";
 import { useEffect, useRef } from "react";
 
-
-
+const _linearVelocity = new Vector3();
 
 export default function Helicopter() {
-  const rigidBodyRef = useRef<RapierRigidBody>(null)
-  const { hoverForce, maxVelocity, hoverHeight, enable } = useControls('helicopter', {
-    hoverForce: {
-      value: 0.02, min: -0.1, max: 0.1
-    },
-    maxVelocity: {
-      value: 0,
-      min: 0, max: 5
-    },
-    hoverHeight: {
-      value: 5,
-      min: 0,
-      max: 10
-    },
-    enable: false,
-  })
-  const position = new Vector3(0, 2, 0)
+  const rigidBodyRef = useRef<RapierRigidBody>(null);
+  const { hoverForce, maxVelocity, hoverHeight, enable, forward } = useControls(
+    "helicopter",
+    {
+      hoverForce: {
+        value: 0.02,
+        min: -0.1,
+        max: 0.1,
+      },
+      maxVelocity: {
+        value: 0,
+        min: 0,
+        max: 5,
+      },
+      hoverHeight: {
+        value: 5,
+        min: 0,
+        max: 10,
+      },
+      enable: false,
+      forward: false,
+    }
+  );
+  const position = new Vector3(0, 2, 0);
   const downVector = new Vector3(0, -1, 0);
   const upVector = new Vector3(0, 1, 0);
+  const forwardVector = new Vector3(0, 0, 1);
   const ray = new Ray(position, downVector);
-  const force = new Vector3()
-  const velocity = new Vector3()
-  const raycaster = new Raycaster()
-  const currentHoverHeight = useRef(0)
+  const currentHoverHeight = useRef(0);
 
-  const { world } = useRapier()
+  const { world } = useRapier();
 
   useEffect(() => {
-    const { current: rigidBody } = rigidBodyRef
+    const { current: rigidBody } = rigidBodyRef;
 
-    if (!rigidBody) return
+    if (!rigidBody) return;
 
-    rigidBody.lockRotations(true, true)
-  }, [])
+    rigidBody.lockRotations(true, true);
+  }, []);
 
   useFrame(() => {
-    const { current: rigidBody } = rigidBodyRef
+    const { current: rigidBody } = rigidBodyRef;
 
-    if (!rigidBody) return
+    if (!rigidBody) return;
 
+    // hover
     ray.origin = rigidBody.translation();
-    ray.dir = downVector.clone()
+    ray.dir = downVector.clone();
     const hit = world.castRay(
       ray,
       hoverHeight,
       true,
       undefined,
       interactionGroups(0, [1])
-    )
+    );
 
     if (hit) {
       const point = new Vector3(
@@ -67,14 +78,22 @@ export default function Helicopter() {
         ray.pointAt(hit.toi).z
       );
 
-
       if (enable) {
-        currentHoverHeight.current = MathUtils.lerp(currentHoverHeight.current, hoverHeight, 0.01);
+        currentHoverHeight.current = MathUtils.lerp(
+          currentHoverHeight.current,
+          hoverHeight,
+          0.01
+        );
       } else {
-        currentHoverHeight.current = MathUtils.lerp(currentHoverHeight.current, 0, 0.0025);
+        currentHoverHeight.current = MathUtils.lerp(
+          currentHoverHeight.current,
+          0,
+          0.0025
+        );
       }
 
-      const diff = currentHoverHeight.current - point.distanceTo(ray.origin as Vector3)
+      const diff =
+        currentHoverHeight.current - point.distanceTo(ray.origin as Vector3);
       const forceVector = upVector
         .clone()
         .normalize()
@@ -83,13 +102,35 @@ export default function Helicopter() {
       rigidBody.applyImpulseAtPoint(forceVector, ray.origin, true);
     }
 
-  })
+    // movement
+    if (forward) {
+      rigidBody.applyImpulse(forwardVector.clone().multiplyScalar(0.04), true);
+    }
+    const angle =
+      0.15 -
+      forwardVector.angleTo(
+        _linearVelocity.copy(rigidBody.linvel() as Vector3)
+      ) *
+        0.1;
+    const q = new Quaternion().setFromEuler(new Euler(angle, 0, 0));
 
+    rigidBody.setRotation(q, true);
+  });
 
-  return <RigidBody ref={rigidBodyRef} collisionGroups={interactionGroups(0, [1])} colliders={false} position={position} restitution={0} mass={10} linearDamping={2}>
-    <CapsuleCollider args={[0.5, 0.25]} rotation={[Math.PI * -0.5, 0, 0]} />
-    <Cone rotation={[Math.PI * -0.5, 0, 0]} args={[0.25, 1, 7]}>
-      <meshNormalMaterial />
-    </Cone>
-  </RigidBody>
+  return (
+    <RigidBody
+      ref={rigidBodyRef}
+      collisionGroups={interactionGroups(0, [1])}
+      colliders={false}
+      position={position}
+      restitution={0}
+      mass={10}
+      linearDamping={2}
+    >
+      <CapsuleCollider args={[0.5, 0.25]} rotation={[Math.PI * 0.5, 0, 0]} />
+      <Cone rotation={[Math.PI * 0.5, 0, 0]} args={[0.25, 1, 7]}>
+        <meshNormalMaterial />
+      </Cone>
+    </RigidBody>
+  );
 }
