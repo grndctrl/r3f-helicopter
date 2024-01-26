32df72eacc19ad7ec9b470bf3d8013ddf80a271a
Mark Brand <mark.brand@icloud.com>
2024-01-26 18:47:21 +0100

    terrain color

--- src/components/Terrain/index.tsx
+++ src/components/Terrain/index.tsx
@@ -8,7 +8,7 @@ import { RigidBody } from "@react-three/rapier";
 export default function Terrain() {
   return <RigidBody colliders="trimesh" type="fixed">
     <Box args={[1000, 1, 1000]} position={[0, -0.5, 0]}>
-      <meshNormalMaterial />
+      <meshBasicMaterial />
     </Box>
   </RigidBody>
 }
